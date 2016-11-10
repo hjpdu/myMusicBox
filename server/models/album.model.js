792cdbc0ad3db5bcce50e8533cb8ce22ce164717
Harryson Pointdujour <Harryson Pointdujour>
2016-11-09 20:29:55 -0500

    created Album model

---
+++ server/models/album.model.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var albumSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+
+  artist:{
+    type: String,
+    required: true
+  },
+
+  ReleaseDate:{
+    type: Date,
+    required: true
+  },
+
+  isGood: {
+    type: Boolean,
+    required: true
+  }
+});
+
+var Album = mongoose.model('Album, albumSchema');
+//the Grand platonic 'Album'  form now exists
+module.exports = Album;
