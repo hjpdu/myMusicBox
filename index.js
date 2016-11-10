6863860a1a6f31ee56a7e790c37dc994c6df7521
Harryson Pointdujour <Harryson Pointdujour>
2016-11-09 20:10:03 -0500

    set up basic index js file and routing to mongoDB

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var server = express();
+var mongoose = require('mongoose');
+var albumRouter = require('./server/routers/album.router.js');
+var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
+
+mongoose.connect(mongoURI);
+
+var port = process.env.PORT || 8080;
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(req,res){
+  res.sendFile('public/html/index.html', {root:__dirname});
+
+});
+
+server.use(albumRouter);
+
+server.listen(port, function(){
+  console.log('Now listening on port...', port);
+});
